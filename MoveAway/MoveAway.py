637b79806b58ee8aaadec3a6fa363575bc5b8856
Leo Lebrun <Boshunter79@gmail.com>
2023-05-26 14:04:52 +0200

    Add files via upload

--- MoveAway/MoveAway.py
+++ MoveAway/MoveAway.py
@@ -1,4 +1,5 @@
 # run the code to generate IR sensor data 
+from shutil import move
 from unifr_api_epuck import wrapper
 import sys, random, signal
 import numpy as np
@@ -28,30 +29,33 @@ if __name__ == "__main__":
     MAX_SPEED = 1.5
     movePhase = 1 # 1 stay on line, 2 move to the right, 3 move back to line
     # wait 3 seconds before startin
+    step = 0
     robot.sleep(3)
 
     while robot.go_on():
+
+        print("Current Phase : " + str(movePhase) + " Step: " + str(step))
         if movePhase == 1:
             if stepcounter % n == 0 :
                 robot.init_camera()
             if stepcounter % n == 1 :
                 img = np.array(robot.get_camera())
-                detections = robot.get_detection(img)
+                detections = robot.get_detection(img, 0.87)
                 for object in detections:
                     if object.label == "Epuck":
-                        print("detected")
-                        robot.set_speed(0, 0)
-                        sendMSG = 1
+                        MAX_SPEED = 0.5
+                        if object.width >= 55:
+                            sendMSG = 1
+                            robot.send_msg(sendMSG)
                     else:
+                        MAX_SPEED = 1.5
                         sendMSG = 0
-                    
-                
                 receivedMSG = ""
                 while robot.has_receive_msg():
                     receivedMSG = robot.receive_msg()
                 if str(receivedMSG) == str(sendMSG) and receivedMSG != 0:
-                    print("Change")
                     movePhase = 2
+                    MAX_SPEED = 1.5
         
             stepcounter += 1
         
@@ -60,21 +64,62 @@ if __name__ == "__main__":
             left = robot.get_ground()[0]
             middle = robot.get_ground()[1]
             right = robot.get_ground()[2]
-    	
-            if left <= 500 and middle <= 500 and right >500:
-                robot.set_speed(MAX_SPEED)
-            elif left <= 500 and middle <= 500 and right <= 500:
-                robot.set_speed(MAX_SPEED * 0.15, MAX_SPEED * (-0.15))
-            elif left >= 500 and middle >= 500 and right <= 500:
-                robot.set_speed(MAX_SPEED * 0.15, MAX_SPEED * (-0.15))
-            elif left >= 500 and middle >= 500 and right >= 500:
-                robot.set_speed(MAX_SPEED * (-0.15), MAX_SPEED * (0.15))
+            #no detection -> turn left
+            if left > 550 and middle > 550 and right > 550:
+                robot.set_speed(MAX_SPEED * (-0.3), MAX_SPEED * (0.3))
+            #only right --> turn right
+            if left > 550 and middle > 550 and right < 550:
+                robot.set_speed(MAX_SPEED * (0.3), MAX_SPEED * (-0.3))
+            #only left --> turn leftstraight
+            if left < 550 and middle > 550 and right > 550:
+                robot.set_speed(MAX_SPEED * (-0.4), MAX_SPEED * (0.4))
+            #only left and middle --> straight
+            if left < 550 and middle < 550 and right > 550:
+                robot.set_speed(MAX_SPEED, MAX_SPEED)
+            #all 3--> turn right
+            if left < 550 and middle < 550 and right < 550:
+                robot.set_speed(MAX_SPEED * (0.3), MAX_SPEED * (-0.3))
+            # only midddle --> right-straight
+            if left > 550 and middle < 550 and right > 550:
+                robot.set_speed(MAX_SPEED * (0.4), MAX_SPEED * (-0.4))
+            #middle and right --> straight-right
+            if left > 550 and middle < 550 and right < 550:
+                robot.set_speed(MAX_SPEED * (0.4), MAX_SPEED*(-0.4))
+            #left and right --> straight-right
+            if left < 550 and middle > 550 and right < 550:
+                robot.set_speed(MAX_SPEED * (0.3), MAX_SPEED*(-0.3))
         elif movePhase == 2:
-        #make it turn around 40° then drive 300 step
-        #afterwards go movePhase 3
-            s = 0
+            
+            if step < 30:
+                robot.set_speed(0.8, -0.8)
+            else:
+                movePhase = 3
+                step = 0
+            step += 1
+
         elif movePhase == 3:
-            #turn 40* to the left then drive until you find the line, once you do 
-            #switch back to movePhase 1
-            s = 0
+            if step < 100:
+                robot.set_speed(MAX_SPEED, MAX_SPEED)
+                step += 1
+            else:
+                step = 0
+                movePhase = 4
+        elif movePhase == 4:
+            if step < 60:
+                robot.set_speed(-0.8, 0.8)
+                step += 1
+            else:
+                movePhase = 5
+                step = 0
+        elif movePhase == 5:
+                left = robot.get_ground()[0]
+                middle = robot.get_ground()[1]
+                right = robot.get_ground()[2]
+                robot.set_speed(MAX_SPEED, MAX_SPEED)
+                if left < 550 or middle < 550 or right < 550:
+                    robot.set_speed(0,0)
+                    movePhase = 1
+                    sendMSG = 0
+                    receivedMSG = 0
+
 robot.clean_up()
